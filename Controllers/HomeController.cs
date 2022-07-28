b03cf3864c245e200c5ed8794aff539a72e37bd6
Christy Hartner <xtyvkv@gmail.com>
2022-07-28 17:45:56 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaupersReddit.Models;
+using System.Diagnostics;
+
+namespace PaupersReddit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayReddit()
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient();
+            const string redditApiUrl = "https://www.reddit.com/r/aww/.json";
+            var apiResponse = httpClient.GetFromJsonAsync<RedditSimpleResponse>(redditApiUrl).GetAwaiter().GetResult();
+            return View(apiResponse);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public class RedditSimpleResponse
+    {
+        public string kind { get; set; }
+        public RedditSimpleResponse_Data data { get; set; }
+    }
+    public class RedditSimpleResponse_Data
+    {
+        public string after { get; set; }
+        public RedditSimpleResponse_Data_Child[] children { get; set; }
+    }
+    public class RedditSimpleResponse_Data_Child
+    {
+        public string kind { get; set; }
+        public RedditSimpleResponse_Data_Child_Data data { get; set; }
+    }
+    public class RedditSimpleResponse_Data_Child_Data
+    {
+        public string title { get; set; }
+        public RedditSimpleResponse_Data_Child_Data_LinkFlairRichText[] link_flair_richtext { get; set; }
+    }
+    public class RedditSimpleResponse_Data_Child_Data_LinkFlairRichText
+    {
+        public string a { get; set; }
+        public string e { get; set; }
+        public string u { get; set; }
+    }
+}
